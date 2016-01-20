c81223aca3fc8e7af3ffe1411e21b01072bb13af
Michael Hirsch <michaelahirsch@gmail.com>
2016-01-20 16:57:57 -0500

    api spike

--- api.py
+++ api.py
@@ -1,28 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, json
 from flask_restful import Resource, Api
 import psycopg2
-import json
+from psycopg2.extras import RealDictCursor
 
 ## Create connection to PSQL
 
 conn = psycopg2.connect("dbname = dev_techinasia user = tableau host = localhost")
+print conn.encoding
+
 
 app = Flask(__name__)
-api = Api(app)
 
-class StartUps_Meta(Resource):
-    def get(self):
-        #Connect to DB
-        cur = conn.cursor()
-        cur.execute("Select distinct tiacompanyid from StartUps")
-        return {"startup_ids": [i[0] for i in cur.fetchall()]}
+@app.route("/")
+def api_root():
+    return "Welcome to the Startup API"
 
-class Industries(Resource):
-    def get(self):
-        return {"hello" : " I'm an industry!"}
-
-api.add_resource(StartUps_Meta, "/startups")
-api.add_resource(Industries, "/industries")
+@app.route("/startups")
+def startups():
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    if 'count' in request.args:
+        count = request.args["count"]
+        query = "Select tiacompanyid, companyname from StartUps limit " + str(count) + ";"
+        cur.execute(query)
+        mike = cur.fetchall()
+        print mike
+        return json.dumps(mike, ensure_ascii=False)
+    else:
+        return "DONT WANT TO PRINT EM ALL"
 
 if __name__ == '__main__':
     app.run()
