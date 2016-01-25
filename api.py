4f44874c76994dc5cbd47ecef927687cb90d8cda
Michael Hirsch <michael.hirsch@rga.com>
2016-01-25 10:39:16 -0500

    moving to api

--- api.py
+++ api.py
@@ -5,11 +5,19 @@ from psycopg2.extras import RealDictCursor
 
 ## Create connection to PSQL
 
-conn = psycopg2.connect("dbname = dev_techinasia user = tableau host = localhost")
+conn = psycopg2.connect("dbname = dev_techinasia user = michaelhi host = localhost")
 print conn.encoding
 
 
 app = Flask(__name__)
+
+#need to enable CORS
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 @app.route("/")
 def api_root():
@@ -20,13 +28,44 @@ def startups():
     cur = conn.cursor(cursor_factory = RealDictCursor)
     if 'count' in request.args:
         count = request.args["count"]
-        query = "Select tiacompanyid, companyname from StartUps limit " + str(count) + ";"
+        query = "SELECT tiacompanyid, companyname FROM StartUps LIMIT " + str(count) + ";"
         cur.execute(query)
         mike = cur.fetchall()
-        print mike
         return json.dumps(mike, ensure_ascii=False)
     else:
         return "DONT WANT TO PRINT EM ALL"
+
+@app.route("/countries")
+def countries():
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    query = """
+        SELECT DISTINCT
+            COUNTRY
+        FROM (
+            SELECT
+                COUNTRY AS COUNTRY
+            FROM STARTUPS
+            UNION ALL
+            SELECT
+                INVESTORLOCATION AS COUNTRY
+            FROM INVESTORS
+            ) as place;
+        """
+    cur.execute(query)
+    mike = cur.fetchall()
+    response = {"countries": [country["country"] for country in mike]}
+    return json.dumps(response, ensure_ascii=False)
+
+@app.route("/industryGraph")
+def industryGraph():
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    if 'industry' in request.args:
+        industry = request.args["industry"].replace("_", " ")
+
+    else:
+        return "PLEASE ADD AN INDUSTRY ARGUMENT"
+
+
 
 if __name__ == '__main__':
     app.run()
